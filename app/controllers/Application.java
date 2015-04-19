66ae1b61fe38e68ebf1bc32ae9a1f68c4c9b81fc
Sebastian Nisser Blanck <sebbenb@gmail.com>
2015-04-19 20:54:22 +0200

    Added login page and removed tests

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,82 +1,32 @@
 package controllers;
 
+import models.*; 
 import play.*;
+import play.data.*;
 import play.mvc.*;
-import java.sql.*;
 import views.html.*;
-import play.db.*;
+
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.libs.Json;
 
 public class Application extends Controller {
-
+    
+  
+    
     public static Result index() {
-        return ok(index.render("WEBSITE IS A GO"));
+         return ok(newpage.render(Form.form(User.class)));
     }
 	
-	public static Result helloWeb() {
-	
-		ObjectNode result = Json.newObject();
-
-		result.put("content", "Hello Web");
-
-		return ok(result);
-	}
-	
-	
-	public static Result getNameAge() {
-		
-		ObjectNode result = Json.newObject();
-		Connection conn = null;
-		Statement stmt = null;
-		
-	    try{
-		
-			conn = DB.getConnection();
-			stmt = conn.createStatement();
-		
-		
-			String sql = "SELECT * FROM test";
-			
-			ResultSet rs = stmt.executeQuery(sql);
-		
-		    while(rs.next()){
-				//Retrieve by column name
-
-				String name  = rs.getString("name");
-				int age = rs.getInt("age");				
-				ObjectNode test = Json.newObject();
-				test.put("Name", name);
-				test.put("Age", age);
-				
-				
-				result.put(name, age);
-		   	}
-		    rs.close();
-
-			return ok(result);
-		}catch(SQLException se){
-			//Handle errors for JDBC
-	        return internalServerError(se.toString());
-		}catch(Exception e){
-	    	//Handle errors for Class.forName
-	        return internalServerError(e.toString());
-	 	}finally{
-			 //finally block used to close resources
-			 try{
-			    if(stmt!=null)
-			       conn.close();
-			 }catch(SQLException se){
-			 }// do nothing
-			 try{
-			    if(conn!=null)
-			       conn.close();
-			 }catch(SQLException se){
-			    return internalServerError(se.toString());
-			 }//end finally try
-	   	}//end try
-
-	}
+	public static Result newPage() {
+       // return ok(newpage.render());
+       return ok();
+    }
+    
+    public static Result authenticate() {
+    Form<User> userForm = Form.form(User.class).bindFromRequest();
+    return ok();
+    }
+    
 }
