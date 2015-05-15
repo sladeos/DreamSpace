416ae19b53f4ba20f2e9e7285ea8c1536ddf45c5
Fredrik S <fredrik.sunnanhed@gmail.com>
2015-05-15 13:48:04 +0200

    hela packlistan

--- app/controllers/PacklistDatabase.java
+++ app/controllers/PacklistDatabase.java
@@ -106,6 +106,27 @@ public static Result addPacklist() {
 		String mobilePhone = json.findPath("mobilePhone").textValue();
 		String camera = json.findPath("camera").textValue();
 		String flashlight = json.findPath("flashlight").textValue();
+		String chair = json.findPath("chair").textValue();
+		String lock = json.findPath("lock").textValue();
+		String installationMedia = json.findPath("installationMedia").textValue();
+		String drivers = json.findPath("drivers").textValue();
+		String games = json.findPath("games").textValue();
+		String antiVirus = json.findPath("antiVirus").textValue();
+		String toothbrush = json.findPath("toothbrush").textValue();
+		String soap = json.findPath("soap").textValue();
+		String handCream = json.findPath("handCream").textValue();
+		String shampoo = json.findPath("shampoo").textValue();
+		String deo = json.findPath("deo").textValue();
+		String aspirin = json.findPath("aspirin").textValue();
+		String earplugs = json.findPath("earplugs").textValue();
+		String medication = json.findPath("medication").textValue();
+		String clothes = json.findPath("clothes").textValue();
+		String swimwear = json.findPath("swimwear").textValue();
+		String sheets = json.findPath("sheets").textValue();
+		String sleepingBag = json.findPath("sleepingBag").textValue();
+		String pillow = json.findPath("pillow").textValue();
+		String mattress = json.findPath("mattress").textValue();
+
 //Testutskrift i activators system bara för att se att 1:orna och 0:orna blir rätt.  
 		System.out.println("ADD -> PC: " + personalComputer + " , " + "POWERSTRIP: " + powerstrip + " , " + "SCREENAND: " + screenAndCables);
 
@@ -113,7 +134,7 @@ public static Result addPacklist() {
 
 			conn = DB.getConnection();
 
-			String insertIntoDatabase = "INSERT INTO Packlist (username, personalComputer, powerstrip, screenAndCables, keyboard, headset, networkCable, screwdriver, cable_Ties, ductTape, gameConsole, mobilePhone, camera, flashlight) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			String insertIntoDatabase = "INSERT INTO Packlist (username, personalComputer, powerstrip, screenAndCables, keyboard, headset, networkCable, screwdriver, cable_Ties, ductTape, gameConsole, mobilePhone, camera, flashlight, chair, lock, installationMedia, drivers, games, antiVirus, toothbrush, soap, handCream, shampoo, deo, aspirin, earplugs, medication, clothes, swimwear, sheets, sleepingBag, pillow, mattress) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 			
 						
 			preparedStatement = conn.prepareStatement(insertIntoDatabase);
@@ -132,6 +153,29 @@ public static Result addPacklist() {
 			preparedStatement.setString(12, mobilePhone);
 			preparedStatement.setString(13, camera);
 			preparedStatement.setString(14, flashlight);
+			preparedStatement.setString(15, chair);
+			preparedStatement.setString(16, lock);
+			preparedStatement.setString(17, installationMedia);
+			preparedStatement.setString(18, drivers);
+			preparedStatement.setString(19, games);
+			preparedStatement.setString(20, antiVirus);
+			preparedStatement.setString(21, toothbrush);
+			preparedStatement.setString(22, soap);
+			preparedStatement.setString(23, handCream);
+			preparedStatement.setString(24, shampoo);
+			preparedStatement.setString(25, deo);
+			preparedStatement.setString(26, aspirin);
+			preparedStatement.setString(27, earplugs);
+			preparedStatement.setString(28, medication);
+			preparedStatement.setString(29, clothes);
+			preparedStatement.setString(30, swimwear);
+			preparedStatement.setString(31, sheets);
+			preparedStatement.setString(32, sleepingBag);
+			preparedStatement.setString(33, pillow);
+			preparedStatement.setString(34, mattress);
+
+
+
 			
 			
 			preparedStatement.executeUpdate();
