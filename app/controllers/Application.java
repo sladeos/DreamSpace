4c798f4650915e53ca03be56a48eab13707994c6
viciousc <sabina.windahl@gmail.com>
2015-05-21 03:17:29 +0200

    joined tournaments på MyProfile

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -159,7 +159,7 @@ public class Application extends Controller {
 		if (user != null) {
 			if (user.equals(userUrl)) {
 				return ok(MyProfile.render(UserProfileDatabase
-						.getProfile(userUrl)));
+						.getProfile(userUrl), TournamentDatabase.getJoinedTournaments()));
 			} else {
 				return ok(OtherUserProfile.render(UserProfileDatabase
 						.getProfile(userUrl)));
@@ -173,7 +173,7 @@ public class Application extends Controller {
 	public static Result getMyProfile() {
 		String user = session("connected");
 		if (user != null) {
-			return ok(MyProfile.render(UserProfileDatabase.getProfile(user)));
+			return ok(MyProfile.render(UserProfileDatabase.getProfile(user), TournamentDatabase.getJoinedTournaments()));
 		} else {
 			return unauthorized(LoginUserPage
 					.render("Welcome, login to explore the website"));
--- app/controllers/UserProfileDatabase.java
+++ app/controllers/UserProfileDatabase.java
@@ -428,7 +428,7 @@ public class UserProfileDatabase extends Controller {
 				preparedStatement.setString(2, currentuser);
 				preparedStatement.executeUpdate();
 
-				return ok(MyProfile.render(UserProfileDatabase.getProfile(currentuser)));
+				return ok(MyProfile.render(UserProfileDatabase.getProfile(currentuser), TournamentDatabase.getJoinedTournaments()));
 
 		} catch (Exception e) {
 			// Handle errors for Class.forName
